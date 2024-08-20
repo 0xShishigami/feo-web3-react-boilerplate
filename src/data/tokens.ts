ec82469e5fea896d3538b48e59622644d72f59c0
0xShishigami <0xshishigami@gmail.com>
2024-08-20 11:59:40 -0300

    fix: remove mainnet tokens

--- src/data/tokens.ts
+++ src/data/tokens.ts
@@ -2,22 +2,10 @@ import { TokenData } from '~/types';
 
 export const TOKEN_LIST: TokenData[] = [
   {
-    address: '0x6B175474E89094C44Da98b954EedeAC495271d0F',
-    chainId: 1,
-    decimals: 18,
-    name: 'DAI',
-  },
-  {
     address: '0x1D70D57ccD2798323232B2dD027B3aBcA5C00091',
     chainId: 11155111,
     decimals: 18,
     name: 'DAI',
-  },
-  {
-    address: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
-    chainId: 1,
-    decimals: 6,
-    name: 'USDC',
   },
   {
     address: '0xC891481A0AaC630F4D89744ccD2C7D2C4215FD47',
