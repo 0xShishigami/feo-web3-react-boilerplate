6db529ca8b98d1d73df5d1be51199220df5cbabb
0xShishigami <0xshishigami@gmail.com>
2024-08-20 11:48:27 -0300

    fix: remove mainnet and localhost

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -8,7 +8,6 @@ import { connectorsForWallets } from '@rainbow-me/rainbowkit';
 import { alchemyUrls, supportedChains } from '~/data';
 import { getConfig } from '../config';
 
-// const { localhost } = wagmiChains;
 const { PROJECT_ID } = getConfig();
 
 const getWallets = () => {
@@ -35,19 +34,12 @@ const connectors = connectorsForWallets(
 const injectedConnector = unstable_connector(injected);
 
 // RPC fallback order: Injected RPC > Alchemy RPC > Public RPC
-const transportsObj: Record<[wagmiChains.Chain, ...wagmiChains.Chain[]][number]['id'], Transport> = Object.fromEntries(
+const transports: Record<[wagmiChains.Chain, ...wagmiChains.Chain[]][number]['id'], Transport> = Object.fromEntries(
   Object.entries(alchemyUrls).map(([chainId, url]) => [chainId, fallback([injectedConnector, http(url), http()])]),
 );
 
-// const isDevEnvironment = !!process && process.env.NODE_ENV === 'development';
-
-// const transports = isDevEnvironment ? { ...transportsObj, [localhost.id]: injectedConnector } : transportsObj;
-const transports = transportsObj;
-// export const availableChains = isDevEnvironment ? [localhost, ...supportedChains] : [...supportedChains];
-export const availableChains = supportedChains;
-
 export const config = createConfig({
-  chains: availableChains as [Chain, ...Chain[]],
+  chains: supportedChains as [Chain, ...Chain[]],
   ssr: true,
   storage: createStorage({
     storage: cookieStorage,
