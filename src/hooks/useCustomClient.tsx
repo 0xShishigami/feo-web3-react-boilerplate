e0886eb14e2abd6fd18b1449c7533235778d7037
0xShishigami <0xshishigami@gmail.com>
2024-08-21 12:25:01 -0300

    fix: error message from context hooks

--- src/hooks/useCustomClient.tsx
+++ src/hooks/useCustomClient.tsx
@@ -5,7 +5,7 @@ export const useCustomClient = () => {
   const context = useContext(CustomClientContext);
 
   if (context === undefined) {
-    throw new Error('useCustomClient must be used within a StateProvider');
+    throw new Error('useCustomClient must be used within a CustomClientProvider');
   }
 
   return context;
--- src/hooks/useToken.tsx
+++ src/hooks/useToken.tsx
@@ -5,7 +5,7 @@ export const useToken = () => {
   const context = useContext(TokenContext);
 
   if (context === undefined) {
-    throw new Error('useToken must be used within a StateProvider');
+    throw new Error('useToken must be used within a TokenProvider');
   }
 
   return context;
